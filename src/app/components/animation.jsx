da575fc52a9b09856a6f8e6802a92c7f5c9d66f9
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-08-05 17:41:53 +0500

    commit everything has been changed

--- src/app/components/animation.jsx
+++ src/app/components/animation.jsx
@@ -5,19 +5,11 @@ const Animation = () => {
   return (
     <div>
       <TypeAnimation
-        className="inline-block bg-gradient-to-r from-[#fff] to-[#fff] bg-clip-text text-transparent"
-        sequence={[
-          "Javohir Yusupov",
-          500,
-          " a frontend developer",
-          500,
-          "Graphic designer",
-          500,
-        ]}
+        className="text-white inline-block bg-gradient-to-r from-[#f79] to-[#f06] bg-clip-text text-transparent text-4xl md:text-7xl"
+        sequence={["Javohir", 1000, "Web developer", 1000]}
         wrapper="span"
         speed={50}
         style={{
-          fontSize: "3em",
           display: "inline-block",
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -11,23 +11,40 @@ export default function Header() {
       <div className="container mx-auto flex items-center justify-between">
         <h1 className="text-5xl font-bold">LOGO</h1>
         <button
-          className="block lg:hidden px-3 py-2 text-white"
+          className="block lg:hidden px-3 py-2 text-white border border-white"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          {isOpen ? (
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
         </button>
         <nav
           className={`lg:flex lg:items-center lg:space-x-6 lg:static absolute top-16 right-0 bg-gray-800 lg:bg-transparent transition-transform duration-300 ease-in-out ${
@@ -36,6 +53,21 @@ export default function Header() {
             isOpen ? "opacity-100" : "opacity-0"
           }`}
         >
+          <button
+            className={`absolute top-4 right-4 lg:hidden px-3 py-2 text-white ${
+              isOpen ? "block" : "hidden"
+            }`}
+            onClick={() => setIsOpen(false)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+            </svg>
+          </button>
           <ul className="flex flex-col lg:flex-row lg:space-x-6">
             <li>
               <Link
