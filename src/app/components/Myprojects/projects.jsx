a671b816acdd0a6a9d884c9d1c3854b5ce0bc66a
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-08-05 18:44:05 +0500

    projects are added

--- src/app/components/Myprojects/projects.jsx
+++ src/app/components/Myprojects/projects.jsx
@@ -1,14 +1,66 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import { FaEye, FaGithub } from "react-icons/fa";
 
 function Projects() {
   const [images, setImages] = useState([]);
 
   const data = {
-    all: ["/first.webp", "/second.webp", "/third.webp", "/four.webp"],
-    web: ["/four.webp","/first.webp" ],
-    mobile: ["/second.webp", "/third.webp"],
+    all: [
+      {
+        id: 1,
+        img: "/travel-journey.png",
+        link: "https://travel-journey-two.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/travel-journey.git",
+      },
+      {
+        id: 2,
+        img: "/project-1.png",
+        link: "https://exam-7-1.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/exam-7.git",
+      },
+      {
+        id: 3,
+        img: "/todo.png",
+        link: "https://todo-typescipt.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/Todo-Typescipt.git",
+      },
+      {
+        id: 4,
+        img: "/calculator1.png",
+        link: "https://calculator-psi-coral.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/calculator.git",
+      },
+    ],
+    web: [
+      {
+        id: 4,
+        img: "/calculator1.png",
+        link: "https://calculator-psi-coral.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/calculator.git",
+      },
+      {
+        id: 3,
+        img: "/todo.png",
+        link: "https://todo-typescipt.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/Todo-Typescipt.git",
+      },
+    ],
+    mobile: [
+      {
+        id: 2,
+        img: "/project-1.png",
+        link: "https://exam-7-1.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/exam-7.git",
+      },
+      {
+        id: 1,
+        img: "/travel-journey.png",
+        link: "https://travel-journey-two.vercel.app/",
+        githubLink: "https://github.com/javohiryusupov77/travel-journey.git",
+      },
+    ],
   };
 
   const handleClick = (category) => {
@@ -38,15 +90,36 @@ function Projects() {
         </button>
       </div>
       <div className="grid grid-cols-2 gap-4">
-        {images.map((url, index) => (
-          <Image
-            width={200}
-            height={200}
-            key={index}
-            src={url}
-            alt={`Project ${index}`}
-            className="w-full h-auto object-cover rounded-lg"
-          />
+        {images.map((project) => (
+          <div key={project.id} className="relative group">
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <Image
+                width={200}
+                height={200}
+                src={project.img}
+                alt={`Project ${project.id}`}
+                className="w-full h-auto object-cover rounded-lg"
+              />
+              <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition bg-black bg-opacity-50 rounded-lg">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white mx-2"
+                >
+                  <FaEye size={24} />
+                </a>
+                <a
+                  href={project.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white mx-2"
+                >
+                  <FaGithub size={24} />
+                </a>
+              </div>
+            </a>
+          </div>
         ))}
       </div>
     </div>
